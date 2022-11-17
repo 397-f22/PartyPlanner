85ae1cd4a1a539f0566da601f673688adc7c2acd
Chris Naing <clnaing23@gmail.com>
2022-11-17 17:27:31 -0600

    restaurant

---
+++ src/components/Restaurant.js
@@ -0,0 +1,49 @@
+
+import React, { useState } from 'react';
+import '../styles/Restaurant.css'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const openLink = (url) => {
+    window.open(url)
+}
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+
+};
+
+const Restaurant = ({ restaurant }) => {
+
+    return (
+        <div className='container-fluid' data-testid='restaurant'>
+            <div className="card" data-testid={`restaurant-${restaurant.TITLE.substring(restaurant.TITLE.indexOf(":") + 1)}`}>
+                <div className="card-title"> {restaurant.TITLE.substring(restaurant.TITLE.indexOf(":") + 1)}
+                </div>
+                <div className="card-category">
+                    {restaurant.DEPARTMENT}
+                </div>
+
+                <div className="card-body">
+                    <div className="row">
+                        <div className='card-text col-sm'> ${restaurant["PAY RATE"][0]}/hr </div>
+                        <div className='card-text col-sm'> {restaurant["WORK ARRANGEMENTS"]} </div>
+                        <div className='card-text col-sm'> {restaurant.LOCATION} </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Restaurant
