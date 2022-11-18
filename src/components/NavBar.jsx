b441f15d1bf1ca4dc669cefff4ebcf9171e8f929
shaoming <xsm90827@gmail.com>
2022-11-17 23:10:38 -0600

    style navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import '../styles/NavBar.css'
 const NavBar = () => {
   return (
     <nav className="nav">
+      <div  class="topbar"> </div>
       <div className="nav-title">
         <span className="nav-title-nu">SeekHosts</span>
       </div>
