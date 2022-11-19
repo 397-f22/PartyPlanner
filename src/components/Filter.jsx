02dd5e3d8822d5b2d721b4a2a5976b7fe8a7de3d
zhenming wang <zhenmingwang@dhcp-10-105-115-137.wireless.northwestern.private>
2022-11-19 17:34:36 -0600

    filter style

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/Filter.css'
+import Icon from './Icon.jsx'
 
 const toggle = (x, lst) =>
   lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst];
@@ -7,10 +8,12 @@ const toggle = (x, lst) =>
 const Filter = ({ filterCategories, setFilterCategories, value, dataTestID }) => {
   return (
     <div className='filter'>
-      <input className="form-check-input" type="checkbox" value={value} data-testid = {dataTestID}
+      <input className="form-check-input" type="checkbox" id={value} value={value} data-testid = {dataTestID}
         onChange={e => setFilterCategories(toggle(e.target.value, filterCategories))} />
-      <label className="form-check-label" htmlFor="flexCheckDefault">
-        {value}
+      <label className="form-check-label" htmlFor={value} >
+      
+      <Icon type={value}></Icon>
+        <p>{value}</p>
       </label>
     </div>
   )
---
+++ src/components/Icon.jsx
@@ -0,0 +1,44 @@
+import '../styles/Icon.css'
+import vegan from "../pics/vegan.png";
+import diary from '../pics/diary.png';
+import gluten from '../pics/gluten-free.png';
+import halal from '../pics/halal.png';
+import kosher from '../pics/kosher.png';
+import nut from '../pics/peanut-free.png';
+import vegetarian from '../pics/vegetarian.png';
+
+
+const Icon = ({type})=>{
+
+    if(type == "Vegan"){
+        return <img style={{width: "36px", height: "36px", display:"block" }} src={vegan} />
+
+    }
+    if(type == "Dairy free"){
+        return <img style={{width: "36px", height: "36px", display:"block"}} src={diary} />
+ 
+    }
+    if(type == "Gluten free"){
+        return <img style={{width: "36px", height: "36px", display:"block"}} src={gluten} />
+     
+    }
+    if(type == "Halal"){
+        return <img style={{width: "36px", height: "36px", display:"block"}} src={halal} />
+     
+    }
+    if(type == "Kosher"){
+        return <img style={{width: "36px", height: "36px", display:"block"}} src={kosher} />
+    }
+    if(type == "Nut Free"){
+        return <img style={{width: "36px", height: "36px", display:"block"}} src={nut} />
+    }
+
+    if(type == "Vegetarian"){
+        return <img style={{width: "36px", height: "36px", display:"block"}} src={vegetarian} />
+    }
+
+
+}
+
+
+export default Icon;
